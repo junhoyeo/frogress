dc3fdf105ba0770270b0bee1e13ccc445375a560
Junho Yeo <hanaro0704@gmail.com>
2021-07-10 22:39:00 +0900

    feat: implement `LineProgressBar` component

--- packages/docs/src/Home/HomePage.tsx
+++ packages/docs/src/Home/HomePage.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { Header } from './Header'
+import { LineProgressBar } from './LineProgressBar'
 
 export const HomePage = () => {
   return (
     <Container>
       <Header />
+      <ProgressList>
+        <LineProgressBar percent={25} />
+        <LineProgressBar percent={65} />
+        <LineProgressBar percent={80} />
+      </ProgressList>
     </Container>
   )
 }
@@ -15,4 +21,15 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`
+
+const ProgressList = styled.div`
+  margin-top: 48px;
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+
+  & > div {
+    margin-top: 16px;
+  }
 `
---
+++ packages/docs/src/Home/LineProgressBar.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// TODO: use declaration in package
+// prettier-ignore
+type StringToCharArray<Str extends string> =
+    string extends Str
+      ? string[]
+      : Str extends ''
+        ? []
+        : Str extends `${infer PartOne}${infer PartTwo}`
+          ? [PartOne, ...StringToCharArray<PartTwo>]: [Str]
+
+// prettier-ignore
+export type Percent<Value extends number>
+  = `${Value}` extends `${infer NumberToString}`
+    ? NumberToString extends `-${string}`
+      // If negative
+      ? never
+      // is Positive
+      : StringToCharArray<NumberToString> extends infer Chars
+        ? Chars extends { length: 1 }
+          // One digit
+          ? Value
+          // Two digit
+          : Chars extends { length: 2 }
+            ? Value
+            // Only 100 is allowed in Three digit numbers
+            : Value extends 100 ? 100 : never
+        : never
+    : never
+
+export type LineProgressBarProps<T extends number> = {
+  percent: Percent<T>
+}
+
+export const LineProgressBar = <T extends number>({
+  percent,
+}: LineProgressBarProps<T>) => {
+  return (
+    <Container>
+      <Progress percent={percent} />
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 32px;
+  background-color: #e9ecef;
+  position: relative;
+  overflow: hidden;
+`
+
+type ProgressProps = {
+  percent: number
+}
+const Progress = styled.div<ProgressProps>`
+  position: absolute;
+  height: 100%;
+  width: calc(${({ percent }) => percent}% + 500px);
+  left: -500px;
+  background-image: linear-gradient(
+    to right,
+    #12d8fa 25%,
+    #43a4ff 85%,
+    #3179ff 98%
+  );
+`
