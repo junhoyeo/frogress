9611f2af8c074035725c63d034395a2e3c9c5b51
Junho Yeo <hanaro0704@gmail.com>
2021-07-12 03:13:09 +0900

    feat(docs): style container of `HighlightedCode`

--- packages/docs/src/components/HighlightedCode.tsx
+++ packages/docs/src/components/HighlightedCode.tsx
@@ -1,6 +1,7 @@
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/nightOwl'
 import React from 'react'
+import styled from 'styled-components'
 
 type HighlightedCodeProps = {
   children: string
@@ -10,18 +11,28 @@ export const HighlightedCode: React.FC<HighlightedCodeProps> = ({
   children,
 }) => {
   return (
-    <Highlight {...defaultProps} theme={theme} code={children} language="tsx">
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+    <Container>
+      <Highlight {...defaultProps} theme={theme} code={children} language="tsx">
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  padding: 4px 16px;
+  border-radius: 8px;
+  background-color: #021727;
+  display: flex;
+  margin-top: 24px;
+`
