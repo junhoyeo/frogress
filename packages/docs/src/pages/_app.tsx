1acac1284a280addd4eee8fe652ae0cfc05c47bd
Junho Yeo <hanaro0704@gmail.com>
2021-07-12 15:09:40 +0900

    feat(docs): add metadata

--- packages/docs/src/pages/_app.tsx
+++ packages/docs/src/pages/_app.tsx
@@ -17,6 +17,11 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>❄️</text></svg>" />
+        <title>Frogress</title>
+        <meta name="description" content="❄️ The ultimate Line Progress Bar UI for React" />
+        <meta property="og:title" content="Frogress" />
+        <meta property="og:description" content="❄️ The ultimate Line Progress Bar UI for React" />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
